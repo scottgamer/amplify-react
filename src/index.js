d25248777e8cc3201aa2ab2ffcae8ec7c3120416
Richard Munoz <richosojason@msn.com>
2020-01-14 22:35:36 -0500

    added amplify config to react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+Amplify.configure(aws_exports);
 
+ReactDOM.render(<App />, document.getElementById("root"));
