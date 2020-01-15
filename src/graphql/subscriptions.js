12cb6a1c9b1a64463d1227f61a24f0a8c762ece7
Richard Munoz <richosojason@msn.com>
2020-01-14 22:28:57 -0500

    updated amplify and node runtime

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = `subscription OnCreateTodo {
+  onCreateTodo {
+    id
+    note
+  }
+}
+`;
+export const onUpdateTodo = `subscription OnUpdateTodo {
+  onUpdateTodo {
+    id
+    note
+  }
+}
+`;
+export const onDeleteTodo = `subscription OnDeleteTodo {
+  onDeleteTodo {
+    id
+    note
+  }
+}
+`;
