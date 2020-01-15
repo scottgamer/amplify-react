019fc1a0b8bacc7d70a931b34574cf7192c2e4d4
Richard Munoz <richosojason@msn.com>
2020-01-14 22:45:52 -0500

    added authentication with withAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 function App() {
   return <div>App</div>;
 }
 
-export default App;
+export default withAuthenticator(App, {
+  includeGreetings: true
+});
